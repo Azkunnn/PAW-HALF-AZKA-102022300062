0560a56c7a5023a36a808229c094f0a6c1f045c8
Azkunnn <azkaassidqi32@gmail.com>
2025-03-08 22:44:25 +0700

    TPMODUL1

--- MODUL1/TPMODUL1_AZKA/soal.php
+++ MODUL1/TPMODUL1_AZKA/soal.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // silakan taruh kode kalian di bawah
 
     if (empty($_POST["nama"])) {
-        $namaErr = "Nama wajib ";
+        $namaErr = "Nama wajib diisi";
     } else {
         $nama = htmlspecialchars($_POST["nama"]);
     }
