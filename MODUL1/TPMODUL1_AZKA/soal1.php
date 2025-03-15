d0cefd33c14b401ab80f63d5ddb8218a5f3a7b08
Azkunnn <azkaassidqi32@gmail.com>
2025-03-15 11:10:53 +0700

    COMMIT JURNAL MODUL 1

--- MODUL1/TPMODUL1_AZKA/soal1.php
+++ MODUL1/TPMODUL1_AZKA/soal1.php
@@ -3,42 +3,35 @@
 $nama = $email = $nim = "";
 $namaErr = $emailErr = $nimErr = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHODE"] == "POST")
 
     // **********************  1  **************************  
     // Tangkap nilai nama yang ada pada form HTML
     // silakan taruh kode kalian di bawah
 
-    if (empty($_POST["nama"])) {
-        $namaErr = "Nama wajib diisi";
-    } else {
-        $nama = htmlspecialchars($_POST["nama"]);
-    }
+if (empty($_POST["nama"])){
+    $namaErr = "Nama wajib diisi";
+} else{
+    $nama = htmlspecialchars($_POST["nama"]);
+}
 
     // **********************  2  **************************  
     // Validasi format email agar sesuai dengan standar
     // silakan taruh kode kalian di bawah
 
-    if (empty($_POST["email"])) {
-        $emailErr = "Email wajib diisi";
-    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        $emailErr = "Format email tidak valid";
-    } else {
-        $email = htmlspecialchars($_POST["email"]);
-    }
+
+    # code...
+$email = $_POST['email'];
+if (empty($email)) {
+    $emailErr = "Email wajib di isi";    # code...
+}    
+}
 
     // **********************  3  **************************  
     // Pastikan NIM terisi dan  angka
     // silakan taruh kode kalian di bawah
 
-    if (empty($_POST["nim"])) {
-        $nimErr = "NIM wajib diisi";
-    } elseif (!ctype_digit($_POST["nim"])) {
-        $nimErr = "NIM harus berupa angka";
-    } else {
-        $nim = htmlspecialchars($_POST["nim"]);
-    }
-}
+    
 ?>
 
 <!DOCTYPE html>
@@ -66,39 +59,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <?php } ?>
         <?php } ?>
 
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
 
          <!-- **********************  4  ************************** -->
          <!-- Tambahkan kolom input untuk Nama, Email, dan NIM dengan mengambil class form-group sebagai referensi -->
 
-            <div class="form-group">
+         <div class="form-group">
                 <!-- tambahkan label nama -->
-                <label for="nama">Nama:</label>
                 <input type="text" id="nama" name="nama" value="<?php echo $nama; ?>">
-                 <!-- Tampilkan pesan error jika variabel $namaErr tidak kosong -->
-                <span class="error"> <?php echo $namaErr; ?></span>
+                <!-- Tampilkan pesan error jika variabel $namaErr tidak kosong -->
             </div>
 
             <div class="form-group">
-                  <!-- tambahkan label email -->
-                <label for="email">Email:</label>
+                <!-- tambahkan label email -->
                 <input type="text" id="email" name="email" value="<?php echo $email; ?>">
-                  <!-- Tampilkan pesan error jika variabel $emailErr tidak kosong -->
-                <span class="error"> <?php echo $emailErr; ?></span>
+                <!-- Tampilkan pesan error jika variabel $emailErr tidak kosong -->
             </div>
 
             <div class="form-group">
                 <!-- tambahkan label nim -->
-                <label for="nim">NIM:</label>
                 <input type="text" id="nim" name="nim" value="<?php echo $nim; ?>">
                 <!-- Tampilkan pesan error jika variabel $nimErr tidak kosong -->
-                <span class="error"> <?php echo $nimErr; ?></span>
             </div>
 
             <div class="button-container">
                 <button type="submit">Daftar</button>
             </div>
+        
+
         </form>
     </div>
 </body>
 </html>
+
